ed45c1d8153ba3bfc29ec24f86ebd364e437a080
oyvikjo <oyvindjohnsen@outlook.com>
2017-01-19 10:32:38 -0800

    Added javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -13,6 +13,14 @@ function initializePage() {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
 
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00")
+
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
